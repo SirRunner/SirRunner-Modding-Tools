9294fa136ac087a9e646ff6b8a5368f117838d37
greeha01 <37346022+greeha01@users.noreply.github.com>
2021-03-06 01:22:53 -0600

    Update terrain_generator.py

--- terrain_detailer/terrain_generator.py
+++ terrain_detailer/terrain_generator.py
@@ -15,7 +15,7 @@ input_file = "C:/Program Files (x86)/Steam/steamapps/common/Victoria 2/mod/TTA/p
 # Changing these variables are optional. By default, the output file will show up in
 # C:\Users\USERNAME\PyCharmProjects\PROJECTNAME\output_folder\output_file
 # output_folder must end with a '/' to ensure that the file is saved in the correct place
-output_folder = "C:\Users\USERNAME\PyCharmProjects\PROJECTNAME\output_folder\output_file"
+output_folder = "C:/Program Files (x86)/Steam/steamapps/common/Victoria 2/mod/TTA/"
 output_file = "terrain_output.bmp"
 
 
