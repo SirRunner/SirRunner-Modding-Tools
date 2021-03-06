589d867234376549a31a7b8e5867db6a84c4d451
greeha01 <37346022+greeha01@users.noreply.github.com>
2021-03-06 01:28:08 -0600

    Final fix. I hope

--- terrain_detailer/terrain_generator.py
+++ terrain_detailer/terrain_generator.py
@@ -13,9 +13,9 @@ from terrain_detailer.src.perlin_terrain_generator import generate_terrain
 input_file = "C:/Program Files (x86)/Steam/steamapps/common/Victoria 2/mod/TTA/pre-terrain-generator.bmp"
 
 # Changing these variables are optional. By default, the output file will show up in
-# C:\Users\USERNAME\PyCharmProjects\PROJECTNAME\output_folder\output_file
+# C:\Users\USERNAME\PyCharmProjects\PROJECTNAME\output\output_file
 # output_folder must end with a '/' to ensure that the file is saved in the correct place
-output_folder = "C:/Program Files (x86)/Steam/steamapps/common/Victoria 2/mod/TTA/"
+output_folder = "C:\Users\USERNAME\PyCharmProjects\PROJECTNAME\output\output_file"
 output_file = "terrain_output.bmp"
 
 
