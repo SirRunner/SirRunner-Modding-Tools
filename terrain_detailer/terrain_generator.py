af3887e435aa3d6d3330a4735147230e7b238b71
greeha01 <37346022+greeha01@users.noreply.github.com>
2021-03-06 01:11:08 -0600

    Update example

--- terrain_detailer/terrain_generator.py
+++ terrain_detailer/terrain_generator.py
@@ -10,12 +10,12 @@ from terrain_detailer.src.perlin_terrain_generator import generate_terrain
 # C:\Users\USERNAME\PyCharmProjects
 # Additionally, you instead could define an absolute path, starting with the drive name:
 # "C:/Program Files (x86)/Steam/steamapps/common/Victoria 2/mod/LOTR/terrain_generation_test_old.bmp"
-input_file = "C:/Program Files (x86)/Steam/steamapps/common/Victoria 2/mod/LOTR/terrain_generation_test_old.bmp"
+input_file = "C:/Program Files (x86)/Steam/steamapps/common/Victoria 2/mod/TTA/pre-terrain-generator.bmp"
 
 # Changing these variables are optional. By default, the output file will show up in
 # C:\Users\USERNAME\PyCharmProjects\PROJECTNAME\output_folder\output_file
 # output_folder must end with a '/' to ensure that the file is saved in the correct place
-output_folder = "../output/"
+output_folder = "C:/Program Files (x86)/Steam/steamapps/common/Victoria 2/mod/TTA/"
 output_file = "terrain_output.bmp"
 
 
